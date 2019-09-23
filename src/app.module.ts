63b09c0f6b3ca776a4615c883771314e20b2601c
adavid <212673582@ge.com>
2019-09-23 18:38:42 +0200

    folders structure ajusted

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
+import { SessionController } from './session/session.controller';
+import { SessionService } from './session/session.service';
+import { SessionModule } from './session/session.module';
+import { QuestionService } from './question/question.service';
+import { ChoiceService } from './choice/choice.service';
+import { QuestionModule } from './question/question.module';
+import { ChoiceModule } from './choice/choice.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/bulkDB'),
-    UserModule],
-  controllers: [AppController],
-  providers: [AppService],
+    UserModule,
+    SessionModule,
+    QuestionModule,
+    ChoiceModule],
+  controllers: [AppController, SessionController],
+  providers: [AppService, SessionService, QuestionService, ChoiceService],
 })
 export class AppModule {}
